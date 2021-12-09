a1a564e135b4ec84e34b60d33e95a6b769c97457
André <94997638+And3e@users.noreply.github.com>
2021-12-09 18:36:47 +0100

    Update random-ascii.cpp

--- ASCII/random-ascii.cpp
+++ ASCII/random-ascii.cpp
@@ -4,8 +4,35 @@
 
 using namespace std;
 
+intestazione(){
+    char const as=201, ad=187, bs=200, bd=188, co=205, cv=186;
+    /*
+    as: alto-sinistra
+    ad: alto-destra
+    bs: basso-sinistra
+    bd: basso-destra
+    co: centro-orrizzontale
+    cv: centro-verticale
+    */
+
+    cout<<as;
+    for(int i=0; i<35; i++){
+        cout<<co;
+    }
+    cout<<ad<<endl;
+    cout<<cv<<"  Generatore random ASCII (0-255)  "<<cv<<endl;
+    cout<<bs;
+    for(int i=0; i<35; i++){
+        cout<<co;
+    }
+    cout<<bd<<endl<<endl;
+}
+
 int main(){
     char cs;
+
+    //Intestazione
+    intestazione();
 
     // cs: carattere speciale
     // tabella ASCII: https://www.oppo.it/tabelle/tabella_ascii.html
