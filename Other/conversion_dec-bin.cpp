85b7646c8ec4b5934871f2a5c60857e63738b90c
André <94997638+And3e@users.noreply.github.com>
2022-09-27 19:28:52 +0200

    Update conversion_dec-bin.cpp

--- Other/conversion_dec-bin.cpp
+++ Other/conversion_dec-bin.cpp
@@ -23,20 +23,20 @@ int main(){
   cin>>n;
 
   while(n>0){ // conversione binario
-	if(n%2==0){
-    a[i]=0;
-	}
-	else{
-    a[i]=1;
-	}
-	n=n/2;
-	i++;
-}
+  	if(n%2==0){
+      a[i]=0;
+    }
+    else{
+      a[i]=1;
+    }
+    n=n/2;
+    i++;
+  }
 
   tot=i;
   i--;
 
-for(int k=0; k<i; k++){ // reverse vettore
+  for(int k=0; k<i; k++){ // reverse vettore
     temp=a[k];
     a[k]=a[i];
     a[i]=temp;
